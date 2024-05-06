a7223c29c71023b1031424817c85cc7d06ff3886
rafi sheik <sheikabdulrafi@gmail.com>
2024-05-06 12:19:50 +0530

    add backend

---
+++ bankmanagement/src/test/java/com/rafisheik/bankmanagement/BankmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafisheik.bankmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
