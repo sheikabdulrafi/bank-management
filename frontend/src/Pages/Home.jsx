3904e155710f1345c5898649376d6f45f7e78937
rafi sheik <sheikabdulrafi@gmail.com>
2024-05-21 13:54:43 +0530

    designed home page

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,9 +1,34 @@
+import Lottie from "lottie-react";
+import globe from "../assets/globeanim.json";
+import security from "../assets/secured.json";
+
 const Home = () => {
   return (
-    <section>
-      <h1>Explore the world of finance with our global banking solutions.</h1>
-      <main>image here</main>
-    </section>
+    <>
+      <section className="homeSection">
+        <div className="content">
+          <h1>
+            Explore the world of finance with our global banking solutions.
+          </h1>
+          <button>Sign in</button>
+        </div>
+        <main>
+          <Lottie animationData={globe} loop={true} reversed={true} />
+        </main>
+      </section>
+      <section className="homeSection ">
+        <main>
+          <Lottie className="secured" animationData={security} loop={true} reversed={true} />
+        </main>
+        <div className="content">
+          <h1>
+            Guarding your credentials with utmost care, our security essentials
+            are always there.
+          </h1>
+          <button>Create Account</button>
+        </div>
+      </section>
+    </>
   );
 };
 
